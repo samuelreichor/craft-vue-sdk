70831733e1b487b2964f35ede261c0bb0b5ee4be
samuelreichor <samuelreichor@gmail.com>
2024-10-13 21:16:32 +0200

    Add workflow, example fetch and update readme

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { useCraftUrlBuilder } from '@craft-vue-sdk';
   import { ref } from 'vue';
+
   const queryEntry = useCraftUrlBuilder('entries')
 
   const queryHomeUrl = queryEntry.slug('home').section('home').buildUrl('all')
@@ -8,6 +9,29 @@
   const queryNewsUrl= ref('')
   function generateNewsUrl() {
     queryNewsUrl.value = queryEntry.section('news').buildUrl('one') 
+  }
+
+  const queryEntryUrl = useCraftUrlBuilder('entries')
+    .slug('home')
+    .section('home')
+    .buildUrl('one');
+
+  try {
+    // Fetch to your craft query api endpoint
+    const response = await fetch(queryEntryUrl);
+    console.log(response)
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from ${queryEntryUrl}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    // Check if your data is there 
+    console.log('Fetched data:', data);
+  } catch (error) {
+    // Handle your error
+    throw new Error(`Could not fetch data from ${queryEntryUrl}`);
   }
 </script>
 
